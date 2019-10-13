e04806e0c1dbd8a93d22ad0dfa62a0fcbb625ad7
Philipp Lackner <philipp.lackner@yahoo.de>
2019-10-13 17:24:13 +0200

    Initial commit

---
+++ app/src/main/java/com/phila/animations/MainActivity.java
@@ -0,0 +1,29 @@
+package com.phila.animations;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_right);
+
+        Button btnPressMe = findViewById(R.id.btnPressMe);
+
+        btnPressMe.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), SecondActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/phila/animations/SecondActivity.java
@@ -0,0 +1,30 @@
+package com.phila.animations;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+
+        overridePendingTransition(R.anim.slide_in_left, R.anim.slide_out_left);
+
+        Button btnBack = findViewById(R.id.btnBack);
+
+        btnBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
